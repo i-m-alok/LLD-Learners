341d1b4a1c8a33e2f5108c5eaa56bcc13e7560a6
Alok Mishra <alok.mishra@walmart.com>
2022-09-24 19:09:44 +0530

    Added file input

--- CarRentalSystem/Sajid/src/com/carrentalsystem/CarRentalApplication.java
+++ CarRentalSystem/Sajid/src/com/carrentalsystem/CarRentalApplication.java
@@ -1,8 +1,46 @@
 package com.carrentalsystem;
 
+import java.io.*;
+
 public class CarRentalApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("./src/com/carrentalsystem/commands.txt");
 
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String line;
+        while (true)
+        {
+            try {
+                if (!((line = br.readLine()) != null)) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(line);
+            String[] arguments = line.split(" ");
+            switch (arguments[0]){
+                case "REGISTER_USER":
+                    System.out.println("User Registerd...");
+                    break;
+                case "REGISTER_CAR":
+                    System.out.println("Car Registered...");
+                    break;
+                case "REGISTER_BIKE":
+                    System.out.println("Bike Registered...");
+                    break;
+                case "BOOK_VEHICLE":
+                    System.out.println("Booked");
+                    break;
+                case "CANCEL_VEHICLE":
+                    System.out.println("Cancle");
+                    break;
+                case "RETURN_VEHICLE":
+                    System.out.println("Return");
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid Operation: "+arguments[0]);
+            }
+        }
     }
 }
